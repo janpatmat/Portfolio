b0f4fa0cc0f23f5815e154df6b6b2ee8777b9fda
Patrick <patmatayabas@gmail.com>
2024-01-05 14:55:55 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './globals/global.scss'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='aws'>Hello world</h1>
+      <h2>Hello</h2>
+      <h3>Hello h3</h3>
+      <h4>Hello h4</h4>
+      <p>Paragraph</p>
+    </>
+  )
+}
+
+export default App
